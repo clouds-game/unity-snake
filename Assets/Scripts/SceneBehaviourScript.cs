1ed08f8f5c6ecc9b38ce3ab442e3c90cf3735552
Clouds Flowing <clouds.flowing@gmail.com>
2022-10-21 05:56:53 +0800

    fix start page

--- Assets/Scripts/SceneBehaviourScript.cs
+++ Assets/Scripts/SceneBehaviourScript.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneBehaviourScript : MonoBehaviour {
+
+  public void Update() {
+    if (name == "StartButton" && Input.GetKeyDown(KeyCode.Space)) {
+      LoadScene("SampleScene");
+    }
+  }
+
   public void LoadScene(string name) {
     SceneManager.LoadScene(name);
   }
